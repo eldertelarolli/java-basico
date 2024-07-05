92531dfb3141eb3db518ee669a13976cebdede6e
eldertelarolli <eldertelarolli@hotmail.com>
2024-07-05 16:47:15 -0300

    alguns tipos de dados

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String Nome = "Elder telarolli";
+        double salarioMinimo = 2500.33;
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+       
+
     }
 }
